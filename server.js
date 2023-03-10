ac10d5adde861ce8caf7b632e9dd5b163da91ebb
KaiChun Chan <kg@bizpro.com.tw>
2023-03-10 17:04:28 +0800

    feat : 新增AI指令 EX: 創 a little dog

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ console.log(`START LISTEN PORT:${PORT}`)
 app.get('/', function (req, res) {
     res.send(`SERVER IS RUNNING ON ${PORT}`);
 });
+
+
+
 const parser = bodyParser.json({
     verify: function (req, res, buf, encoding) {
         req.rawBody = buf.toString(encoding);
@@ -33,20 +36,35 @@ app.post('/webhook', parser, function (req, res) {
     bot.parse(req.body);
     return res.send({});
 });
+
+
+
 bot.on('message', function (event) {
     console.log(event.message.text)
     try {
         if (event.message.text.includes("欸")) {
-            model.requestChat(event.message.text).then((replay) => {
+            var eventMessage = event.message.text.replace('欸', '')
+            model.requestChat(eventMessage).then((replay) => {
                 event.reply(replay.toString())
             }).catch((err) => {
                 console.log(err)
                 event.reply('抱歉，我暫時無法操作。');
+            });
+        } else if (event.message.text.includes("創")) {
+            var eventMessage = event.message.text.replace('創', '')
+            model.requestImage(eventMessage).then((replay) => {
+                event.reply({
+                    type: 'image',
+                    originalContentUrl: replay,
+                    previewImageUrl: replay
+                })
+            }).catch((err) => {
+                console.log(err)
+                event.reply('抱歉，我暫時無法創造圖片。');
             });
         }
     } catch (error) {
         console.log(error)
         event.reply('抱歉，我暫時無法操作。');
     }
-
 });
--- src/model/requestModel.js
+++ src/model/requestModel.js
@@ -1,31 +1,8 @@
 const request = require('request');
+var webClient = require('https')
 require('dotenv').config();
 
 const KEY = process.env.OPENAI_API_KEY;
-// function requestChat(content) {
-//     const options = {
-//         url: 'https://api.openai.com/v1/chat/completions',
-//         json: true,
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${KEY}`
-//         },
-//         body: {
-//             "model": "gpt-3.5-turbo-0301",
-//             "messages": [{ "role": "user", "content": content }]
-//         }
-//     };
-
-//     request.post(options, (err, res, body) => {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log(`Status: ${res.statusCode}`);
-//         const replyMessage = body.choices[0].message.content;
-//         console.log(replyMessage);
-//         return replyMessage
-//     });
-// }
 
 exports.requestChat = function (content) {
     const options = {
@@ -53,6 +30,41 @@ exports.requestChat = function (content) {
                 const replyMessage = body.choices[0].message.content;
                 console.log(replyMessage);
                 resolve(replyMessage)
+            } catch (err) {
+                reject(err)
+            }
+
+        });
+    });
+
+}
+
+exports.requestImage = function (content) {
+    const options = {
+        url: 'https://api.openai.com/v1/images/generations',
+        json: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${KEY}`
+        },
+        body: {
+            "prompt": content,
+            "n": 1,
+            "size": "512x512"
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        request.post(options, (err, res, body) => {
+            if (err) {
+                reject("失敗")
+                console.log(err)
+            }
+            // console.log(body)
+            try {
+                const replyWebUrl = body.data[0].url;
+                console.log(replyWebUrl);
+                resolve(replyWebUrl)
             } catch (err) {
                 reject(err)
             }
