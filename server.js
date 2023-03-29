5ab7b2081e60f4e709d387709cbc3dd059e6d9a4
KaiChun Chan <kg@bizpro.com.tw>
2023-03-29 16:25:44 +0800

    refactor : 針對undefined不處理

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+// model.requestChat('可以用嗎').then((replay) => {
+//     // event.reply(replay.toString())
+// }).catch((err) => {
+//     console.log('抱歉，我暫時無法操作。')
+//     // event.reply('抱歉，我暫時無法操作。');
+// });
+
+
 let bot = linebot({
     channelId: process.env.LINE_CHANNELID,
     channelSecret: process.env.LINE_CHANNELSECRET,
     channelAccessToken: process.env.LINE__ACCESS_TOKEN
 });
-
-
 
 
 const PORT = 8080
@@ -41,30 +48,33 @@ app.post('/webhook', parser, function (req, res) {
 
 bot.on('message', function (event) {
     console.log(event.message.text)
-    try {
-        if (event.message.text.includes("欸")) {
-            var eventMessage = event.message.text.replace('欸', '')
-            model.requestChat(eventMessage).then((replay) => {
-                event.reply(replay.toString())
-            }).catch((err) => {
-                console.log(err)
-                event.reply('抱歉，我暫時無法操作。');
-            });
-        } else if (event.message.text.includes("創")) {
-            var eventMessage = event.message.text.replace('創', '')
-            model.requestImage(eventMessage).then((replay) => {
-                event.reply({
-                    type: 'image',
-                    originalContentUrl: replay,
-                    previewImageUrl: replay
-                })
-            }).catch((err) => {
-                console.log(err)
-                event.reply('抱歉，我暫時無法創造圖片。');
-            });
+    if (event.message.text != undefined) {
+        try {
+            if (event.message.text.includes("欸")) {
+                var eventMessage = event.message.text.replace('欸', '')
+                model.requestChat(eventMessage).then((replay) => {
+                    event.reply(replay.toString())
+                }).catch((err) => {
+                    console.log(err)
+                    event.reply('抱歉，我暫時無法操作。');
+                });
+            } else if (event.message.text.includes("創")) {
+                var eventMessage = event.message.text.replace('創', '')
+                model.requestImage(eventMessage).then((replay) => {
+                    event.reply({
+                        type: 'image',
+                        originalContentUrl: replay,
+                        previewImageUrl: replay
+                    })
+                }).catch((err) => {
+                    console.log(err)
+                    event.reply('抱歉，我暫時無法創造圖片。');
+                });
+            }
+        } catch (error) {
+            console.log(error)
+            event.reply('抱歉，我暫時無法操作。');
         }
-    } catch (error) {
-        console.log(error)
-        event.reply('抱歉，我暫時無法操作。');
     }
+
 });
--- src/model/requestModel.js
+++ src/model/requestModel.js
@@ -12,7 +12,7 @@ exports.requestChat = function (content) {
             'Authorization': `Bearer ${KEY}`
         },
         body: {
-            "model": "gpt-3.5-turbo-0301", // gpt-3.5-turbo or gpt-3.5-turbo-0301
+            "model": "gpt-3.5-turbo", // https://platform.openai.com/docs/models/model-endpoint-compatibility
             "messages": [{ "role": "user", "content": content }]
         }
     };
